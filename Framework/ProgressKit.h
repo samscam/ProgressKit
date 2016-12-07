ab70c93ae3fc34d0178048c6527de86fc8e76aec
Sam Easterby-Smith <me@samscam.co.uk>
2016-12-07 11:55:18 +0000

    Added framework taget

---
+++ Framework/ProgressKit.h
@@ -0,0 +1,19 @@
+//
+//  ProgressKit-macOS.h
+//  ProgressKit-macOS
+//
+//  Created by Sam Easterby-Smith on 07/12/2016.
+//  Copyright © 2016 Kauntey Suryawanshi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ProgressKit-macOS.
+FOUNDATION_EXPORT double ProgressKit_macOSVersionNumber;
+
+//! Project version string for ProgressKit-macOS.
+FOUNDATION_EXPORT const unsigned char ProgressKit_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProgressKit_macOS/PublicHeader.h>
+
+
